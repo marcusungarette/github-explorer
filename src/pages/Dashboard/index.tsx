31f5e71c6bb25bf01330b413e0d68991d63cb8fc
marcusungarette <marcusungarette@gmail.com>
2020-07-01 02:04:49 -0300

    Setting routes and global styles

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Title } from './styles';
+
+const Dashboard: React.FC = () => {
+  return <Title>Explore repositórios no Github</Title>;
+};
+
+export default Dashboard;
