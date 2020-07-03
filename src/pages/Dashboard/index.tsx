a709cc96765f1baa4c55a58428544c6d99516b43
marcusungarette <marcusungarette@gmail.com>
2020-07-03 04:53:23 -0300

    Add Ancor tag / Form tag / react-icons and styled all elements

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,58 @@
 import React from 'react';
-import { Title } from './styles';
+
+import { FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+      <Form>
+        <input placeholder="Digite aqui" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/32929725?s=400&u=552e962be866a27f63fbce30ce221a95da70f5d5&v=4"
+            alt="Github Marcus Ungarette"
+          />
+          <div>
+            <strong>marcusungarette/portfolioweb</strong>
+            <p>Meus trabalhos mais recentes.</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/32929725?s=400&u=552e962be866a27f63fbce30ce221a95da70f5d5&v=4"
+            alt="Github Marcus Ungarette"
+          />
+          <div>
+            <strong>marcusungarette/portfolioweb</strong>
+            <p>Meus trabalhos mais recentes.</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/32929725?s=400&u=552e962be866a27f63fbce30ce221a95da70f5d5&v=4"
+            alt="Github Marcus Ungarette"
+          />
+          <div>
+            <strong>marcusungarette/portfolioweb</strong>
+            <p>Meus trabalhos mais recentes.</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
