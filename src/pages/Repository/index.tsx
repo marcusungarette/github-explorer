8db7b67110b1230ef9790f7d6ca0f5386fc40896
marcusungarette <marcusungarette@gmail.com>
2020-07-04 23:15:44 -0300

    Listed Routes; created components Repository; add styled;

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,64 @@
 import React from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronsLeft, FiChevronRight } from 'react-icons/fi';
+import { Header, RepositoryInfo, Issues } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+interface RepositoryParams {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
+  const { params } = useRouteMatch<RepositoryParams>();
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronsLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars3.githubusercontent.com/u/32929725?v=4"
+            alt="Marcus Ungarette"
+          />
+          <div>
+            <strong>marcusungarette/portfolioweb</strong>
+            <p>My Portfolio</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1908</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>45</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>32</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="dasdsa">
+          <div>
+            <strong>dsadasdsa</strong>
+            <p>dasda</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
