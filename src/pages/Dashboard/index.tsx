b31ac78e136e51e4b8eb11debc8c805ddd359be8
marcusungarette <marcusungarette@gmail.com>
2020-07-04 21:06:47 -0300

    LocalStorage function added

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 import api from '../../services/api';
 
@@ -17,13 +17,28 @@ interface Repository {
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('');
   const [inputError, setInputError] = useState('');
-  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storagedRepositories = localStorage.getItem(
+      '@GithubExplorer:repositories',
+    );
+    if (storagedRepositories) {
+      return JSON.parse(storagedRepositories);
+    }
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem(
+      '@GithubExplorer:repositories',
+      JSON.stringify(repositories),
+    );
+  }, [repositories]);
 
   async function handleAddRepositoriy(
     event: FormEvent<HTMLFormElement>,
   ): Promise<void> {
     event.preventDefault();
-
+    // Verificar se a variavel newRepo esta vazia
     if (!newRepo) {
       setInputError('Digite autor/nome do repositorio');
       return;
@@ -35,7 +50,7 @@ const Dashboard: React.FC = () => {
       const repository = response.data;
       setRepositories([...repositories, repository]);
       setNewRepo('');
-      setInputError('');
+      setInputError(''); // Remove a msg de erro
     } catch (err) {
       setInputError('Erro ao buscar esse repositorio');
     }
